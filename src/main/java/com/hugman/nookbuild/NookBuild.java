af478a3d30d970f320584546bfa0f36afc9ddbe1
Hugman <contact.hugman@gmail.com>
2020-08-01 15:18:17 +0200

    Added initial files

---
+++ src/main/java/com/hugman/nookbuild/NookBuild.java
@@ -0,0 +1,15 @@
+package com.hugman.nookbuild;
+
+import com.hugman.dawn.api.creator.ModData;
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class NookBuild implements ModInitializer {
+	public static final ModData MOD_DATA = new ModData("nookbuild");
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	@Override
+	public void onInitialize() {
+	}
+}
